bf045b237c82d108e3d0782acab8397679c32383
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-05-13 17:26:19 +0300

    3 test

--- VilenDsh/src/App.vue
+++ VilenDsh/src/App.vue
@@ -1,20 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+	export default {
+	name: 'App',
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<div>
+		Hello World
+    <p> My name is Vilen</p>
+	</div>
 </template>
 
 <style scoped>
